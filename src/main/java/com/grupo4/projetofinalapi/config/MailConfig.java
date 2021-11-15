7230ad08507070a0f00030e301c654f05aab29fb
Lucas Vihuchi <lucasvihuchi@gmail.com>
2021-11-15 11:39:08 -0300

    CRUD revisado

--- src/main/java/com/grupo4/projetofinalapi/config/MailConfig.java
+++ src/main/java/com/grupo4/projetofinalapi/config/MailConfig.java
@@ -2,20 +2,24 @@ package com.grupo4.projetofinalapi.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Configuration
 public class MailConfig {
     @Autowired
     public JavaMailSender javaMailSender;
 
-    public void sendMail(String para, String assunto, String conteudo) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("grupo4.serratec@gmail.com");
-        message.setTo(para);
-        message.setSubject(assunto);
-        message.setText(conteudo);
+    public void sendMail(String para, String assunto, String conteudo) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, "utf-8");
+        helper.setFrom("grupo4.serratec@gmail.com");
+        helper.setTo(para);
+        helper.setSubject(assunto);
+        helper.setText(conteudo, true);
         javaMailSender.send(message);
     }
 }
